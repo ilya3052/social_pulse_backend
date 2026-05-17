fffd4490d88e0784e1b3cbc03728e6500e4f185b
ilya3052 <ipopadin5@gmail.com>
2026-05-17 22:04:04 +0300

    создание модели отчета

--- src/reports/models.py
+++ src/reports/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+class Report(models.Model):
+    filename = models.CharField(max_length=256)
+    path = models.FilePathField()
+    date = models.DateTimeField(default=timezone.now)
+    type = models.IntegerField()
+    group = models.ForeignKey('social_entities.Group', on_delete=models.DO_NOTHING, null=True, blank=True,
+                              related_name='report')
+    user = models.OneToOneField('users.CustomUser', on_delete=models.CASCADE, related_name='group_reports')
