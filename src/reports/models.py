da4a9b8b89de528ab2f8c2f5d77a9f7863e878a9
ilya3052 <ipopadin5@gmail.com>
2026-05-18 10:04:11 +0300

    изменение типов переменных модели отчетов

--- src/reports/models.py
+++ src/reports/models.py
@@ -2,10 +2,14 @@ from django.db import models
 from django.utils import timezone
 
 class Report(models.Model):
+    report_type = {
+        "XLSX": "XLSX",
+        "PDF": "PDF"
+    }
     filename = models.CharField(max_length=256)
-    path = models.FilePathField()
+    path = models.CharField(max_length=512)
     date = models.DateTimeField(default=timezone.now)
-    type = models.IntegerField()
+    type = models.CharField(max_length=4, choices=report_type)
     group = models.ForeignKey('social_entities.Group', on_delete=models.DO_NOTHING, null=True, blank=True,
                               related_name='report')
-    user = models.OneToOneField('users.CustomUser', on_delete=models.CASCADE, related_name='group_reports')
+    user = models.ForeignKey('users.CustomUser', on_delete=models.CASCADE, related_name='group_reports')
