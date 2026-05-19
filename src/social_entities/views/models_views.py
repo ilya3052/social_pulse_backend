ec70ffdc2ae2922fd27f2f0e3904c366b0ff69b1
ilya3052 <ipopadin5@gmail.com>
2026-05-19 16:17:20 +0300

    миграции

---
+++ src/social_entities/views/models_views.py
@@ -0,0 +1,15 @@
+from rest_framework.response import Response
+from rest_framework.views import APIView
+import json
+from social_entities.models import PredictiveModels
+
+
+class PredictiveModelsView(APIView):
+    # permission_classes = [IsAuthenticated]
+
+    def get(self, request, *args, **kwargs):
+        model = PredictiveModels.objects.create(model=PredictiveModels.RIDGE, fields=json.dumps({"views_count": 100, "likes": 250}))
+
+        fields = json.loads(model.params)
+        print(fields, type(fields))
+        return Response(200)
