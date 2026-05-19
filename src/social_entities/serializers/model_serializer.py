f467091d357084e3d8b14a44bc9a7438e4f6e408
ilya3052 <ipopadin5@gmail.com>
2026-05-19 16:18:29 +0300

    создание сериализатора для предсказательной модели

---
+++ src/social_entities/serializers/model_serializer.py
@@ -0,0 +1,17 @@
+from rest_framework import serializers
+
+from social_entities.models import PredictiveModels
+
+
+class PredictiveModelSerializer(serializers.ModelSerializer):
+    def get_fields(self):
+        fields = super().get_fields()
+        exclude_fields = self.context.get('exclude_fields', [])
+
+        for field in exclude_fields:
+            fields.pop(field, None)
+        return fields
+    
+    class Meta:
+        model = PredictiveModels
+        fields = '__all__'
