6ac706b352d6a1883cab8aa76a28de2acb1d9cc5
ilya3052 <ipopadin5@gmail.com>
2026-05-30 14:03:54 +0300

    создание эндпоинта для получения отчетов пользователя

--- src/reports/urls.py
+++ src/reports/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from reports.views import AdminReportPDFView, GroupReportsView, CompareGroupReportView
+from reports.views import AdminReportPDFView, GroupReportsView, CompareGroupReportView, UserReportsView
 
 urlpatterns = [
+    path('', UserReportsView.as_view({"get": "list"}), name='reports'),
     path('admin/', AdminReportPDFView.as_view(), name='admin_report'),
     path('group/<int:pk>/', GroupReportsView.as_view(), name='group_report'),
     path('compare/', CompareGroupReportView.as_view(), name='compare_report'),
