a11eebda2bd4b732571f1301d268da1c5bb58812
ilya3052 <ipopadin5@gmail.com>
2026-05-30 14:02:24 +0300

    изменение нейминга полей и добавление поля типа отчета (по группе или сравнительный)

--- src/reports/models.py
+++ src/reports/models.py
@@ -3,14 +3,19 @@ from django.utils import timezone
 
 
 class Report(models.Model):
-    report_type = {
+    report_format = {
         "XLSX": "XLSX",
         "PDF": "PDF"
+    }
+    report_type = {
+        'by_group': 'by_group',
+        'comparative': 'comparative',
     }
     filename = models.CharField(max_length=256)
     path = models.CharField(max_length=512)
     date = models.DateTimeField(default=timezone.now)
-    type = models.CharField(max_length=4, choices=report_type)
+    format = models.CharField(max_length=4, choices=report_format)
+    type = models.CharField(max_length=16, choices=report_type)
     group = models.ForeignKey('social_entities.Group', on_delete=models.DO_NOTHING, null=True, blank=True,
                               related_name='report')
     user = models.ForeignKey('users.CustomUser', on_delete=models.CASCADE, related_name='group_reports')
