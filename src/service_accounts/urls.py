517591885025f1c4c12c4f76dc644432c1d78f8b
ilya3052 <ipopadin5@gmail.com>
2026-06-01 20:29:38 +0300

    удаление url и методов во view отпавших за ненадобностью после изменения логики удаления сервисного аккаунта

--- src/service_accounts/urls.py
+++ src/service_accounts/urls.py
@@ -5,10 +5,6 @@ from service_accounts.views import ServiceAccountsView, ServiceAccountActivateVi
 urlpatterns = [
     path('all/', ServiceAccountsView.as_view({"get": "list"}), name='get-service-accounts-info'),
     path('activate/<int:account_id>', ServiceAccountActivateView.as_view(), name='activate-service-account'),
-    path('<int:pk>',
-         ServiceAccountsView.as_view({"get": "get_with_groups", "patch": "partial_update", "delete": "destroy"}),
-         name='update-service-account'),
-    path('<str:platform>', ServiceAccountsView.as_view({"get": "retrieve"}),
-         name='get-service-account'),
+    path('<int:pk>', ServiceAccountsView.as_view({"delete": "destroy"}), name='delete-service-account'),
     path('', ServiceAccountsView.as_view({"post": "create"}), name='create-service-account'),
 ]
