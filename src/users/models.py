45c879cfd588be29e58a6c689e8acc7e3c86684b
ilya3052 <ipopadin5@gmail.com>
2026-06-10 22:35:40 +0300

    установка индекса на email

--- src/users/models.py
+++ src/users/models.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
 
 class CustomUser(AbstractUser):
@@ -8,4 +9,5 @@ class CustomUser(AbstractUser):
     vk_link = models.CharField(max_length=255, blank=True, null=True)
     vk_id = models.IntegerField(blank=True, null=True)
     is_email_confirmed = models.BooleanField(default=False)
+    email = models.EmailField(_("email address"), blank=True, unique=True, db_index=True)
     groups = models.ManyToManyField('social_entities.Group')
