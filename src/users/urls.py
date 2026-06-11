d6cfeabf36a4da1124e178403ad5451a2254cde1
ilya3052 <ipopadin5@gmail.com>
2026-06-11 13:37:20 +0300

    исправление url

--- src/users/urls.py
+++ src/users/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
     path('register/', UserAPIRegistration.as_view(), name='register'),
 
     path(r'me/', UserAPIView.as_view(), name='me'),
-    path('undind-social/', UnbindSocialView.as_view(), name='unbind_social'),
+    path('unbind-social/', UnbindSocialView.as_view(), name='unbind_social'),
     path('change-password/', UserChangePasswordView.as_view(), name='change_password'),
     path('set-password/', UserSetPasswordView.as_view(), name='set_password'),
     path('get-social/', UserSocialDataView.as_view(), name='user-social-data'),
