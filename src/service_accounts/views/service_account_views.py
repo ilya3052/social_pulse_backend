771d813ce65beee6943ff06b3af1a76a1319dcc5
ilya3052 <ipopadin5@gmail.com>
2026-06-12 20:50:07 +0300

    разделение одного большого view на несколько маленьких каждый из которых выполняет свои определенные действия

--- src/service_accounts/views/service_account_views.py
+++ src/service_accounts/views/service_account_views.py
@@ -3,27 +3,17 @@ from heapq import heapify, heappop, heappush
 from secrets import token_hex
 
 from django.db.models import Count, Q
-from rest_framework import viewsets, status
-from rest_framework.permissions import IsAuthenticated, IsAdminUser
+from rest_framework import status, generics
+from rest_framework.permissions import IsAdminUser, IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
 from service_accounts.models import ServiceAccount, OneTimeToken, ServiceAccountData
-from service_accounts.permissions import ReadOnly
 from service_accounts.serializers import ServiceAccountSerializer
 from social_entities.models import Group
 
 
-class ServiceAccountsView(viewsets.ModelViewSet):
-    def get_permissions(self):
-        if self.action == 'retrieve':
-            permission_classes = [IsAuthenticated]
-        elif self.action in ('list', 'create', 'partial_update', 'destroy', 'get_with_groups'):
-            permission_classes = [IsAdminUser]
-        else:
-            permission_classes = [ReadOnly]
-        return [permission() for permission in permission_classes]
-
+class ServiceAccountsViewMixin:
     def get_queryset(self):
         return (ServiceAccount.objects
                 .select_related('data')
@@ -32,6 +22,29 @@ class ServiceAccountsView(viewsets.ModelViewSet):
                 .all())
 
     pagination_class = None
+    serializer_class = ServiceAccountSerializer
+
+
+class ServiceAccountsListView(ServiceAccountsViewMixin, generics.ListAPIView):
+    permission_classes = [IsAdminUser]
+
+    def list(self, request, *args, **kwargs):
+        accounts = self.get_queryset().annotate(groups_count=Count('groups'))
+        context = {
+            'exclude_fields': [
+                'data', 'groups', 'platform_id', 'app_id'
+            ]
+        }
+        from social_entities.services import get_group_aggregated_info
+        group_data = get_group_aggregated_info()
+        serializer = self.get_serializer(accounts, many=True, context=context)
+        return Response(
+            {"data": serializer.data, "total_group_count": group_data.get('vk_count') + group_data.get('tg_count')},
+            status=status.HTTP_200_OK)
+
+
+class ServiceAccountsUpdateDeleteView(ServiceAccountsViewMixin, generics.UpdateAPIView, generics.DestroyAPIView):
+    permission_classes = [IsAdminUser]
 
     def destroy(self, request, *args, **kwargs):
         instance = self.get_object()
@@ -61,12 +74,16 @@ class ServiceAccountsView(viewsets.ModelViewSet):
         if session_path := account_data.session_path:
             if os.path.exists(session_path):
                 os.remove(session_path)
-        return super().destroy(request, *args, *kwargs)
+        return super().destroy(request, *args, **kwargs)
+
+
+class ServiceAccountRetrieveView(ServiceAccountsViewMixin, generics.RetrieveAPIView):
+    permission_classes = [IsAuthenticated]
 
     def retrieve(self, request, *args, **kwargs):
         account = (
-            ServiceAccount.objects.filter(platform__alias=self.kwargs.get('platform'))
-            .prefetch_related('groups')
+            self.get_queryset()
+            .filter(platform__alias=self.kwargs.get('platform'))
             .annotate(
                 groups_count=Count('groups')
             )
@@ -82,47 +99,12 @@ class ServiceAccountsView(viewsets.ModelViewSet):
             ]
         }
 
-        serializer = ServiceAccountSerializer(account, context=context)
+        serializer = self.get_serializer(account, context=context)
         return Response(serializer.data, status=status.HTTP_200_OK)
 
-    def list(self, request, *args, **kwargs):
-        accounts = (
-            ServiceAccount.objects.all()
-            .annotate(
-                groups_count=Count('groups')
-            )
-        )
 
-        context = {
-            'exclude_fields': [
-                'data', 'groups', 'platform_id', 'app_id'
-            ]
-        }
-        from social_entities.services import get_group_aggregated_info
-        group_data = get_group_aggregated_info()
-        serializer = ServiceAccountSerializer(accounts, many=True, context=context)
-        return Response(
-            {"data": serializer.data, "total_group_count": group_data.get('vk_count') + group_data.get('tg_count')},
-            status=status.HTTP_200_OK)
-
-    def partial_update(self, request, *args, **kwargs):
-        instance = ServiceAccount.objects.get(pk=self.kwargs.get('pk'))
-        if not instance:
-            return Response({"msg": "Объект не найден"}, status=status.HTTP_404_NOT_FOUND)
-        serializer = ServiceAccountSerializer(instance, data=request.data, partial=True)
-
-        if not serializer.is_valid():
-            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-        serializer.save()
-        return Response(serializer.data, status=status.HTTP_200_OK)
-
-    def create(self, request, *args, **kwargs):
-        serializer = ServiceAccountSerializer(data=request.data)
-        if not serializer.is_valid():
-            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-        serializer.save()
-        return Response(status=status.HTTP_201_CREATED)
+class ServiceAccountCreateView(ServiceAccountsViewMixin, generics.CreateAPIView):
+    permission_classes = [IsAdminUser]
 
 
 class ServiceAccountActivateView(APIView):
