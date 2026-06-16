eea9b58273113eab65fd880f1aed42aaf8914bdc
ilya3052 <ipopadin5@gmail.com>
2026-06-16 11:39:27 +0300

    исправление ошибки импортов

--- src/stats/urls.py
+++ src/stats/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from stats.views import BestPostsView, SnapshotView
+from stats.views import best_posts_view, snapshot_view
 
 urlpatterns = [
     path('<int:group_id>/best/', best_posts_view),
